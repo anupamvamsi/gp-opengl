d3f9e2af3c8640b1564251a14fe4d609143dba2f
Anupam Vamsi <39518444+anupamvamsi@users.noreply.github.com>
2024-02-11 20:49:37 +0530

    Hello-Window: Change formatting to Mozilla

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,110 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
 // GLFW: Callbacks
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-void processInput(GLFWwindow *window);
+void
+processInput(GLFWwindow* window);
 
-int main()
+int
+main()
 {
-    // GLFW Init
-    // ---------
-    if (!glfwInit())
-    {
-        std::cout << "Failed to initialize GLFW!" << std::endl;
-        return -1;
-    }
+  // GLFW Init
+  // ---------
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW!" << std::endl;
+    return -1;
+  }
 
-    // Set OpenGL version and Profile (Immediate vs. Core)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // Set OpenGL version and Profile (Immediate vs. Core)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // GLFW Window Init
-    // ----------------
-    const int WINDOW_WIDTH = 1024;
-    const int WINDOW_HEIGHT = 768;
+  // GLFW Window Init
+  // ----------------
+  const int WINDOW_WIDTH = 1024;
+  const int WINDOW_HEIGHT = 768;
 
-    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "learning openGL", nullptr, nullptr);
-    if (window == nullptr)
-    {
-        std::cout << "Failed to create GLFW Window!" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    // Tell GLFW to make the window context the main context on the current thread.
-    glfwMakeContextCurrent(window);
-
-    // Registering cb function for window resize: cb function called every time a window is resized.
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // GLAD Init - Load all OpenGL function pointers
-    // ---------------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD!" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    // Registration of callback functions is done after window creation and before the render loop initiation.
-    // -------------------------------------------------------------------------------------------------------
-
-    // Render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // Input
-        // -----
-        processInput(window);
-
-        // Rendering
-        // ---------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap buffers and poll IO events (key press/release, mouse events, etc.)
-        // -----------------------------------------------------------------------
-        glfwSwapBuffers(window); // swap between front and back buffers? to prevent render flickering issues
-        glfwPollEvents();
-    }
-
-    // Clean up GLFW resources
-    // -----------------------
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH, WINDOW_HEIGHT, "learning openGL", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW Window!" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+  // Tell GLFW to make the window context the main context on the current
+  // thread.
+  glfwMakeContextCurrent(window);
+
+  // Registering cb function for window resize: cb function called every time a
+  // window is resized.
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // GLAD Init - Load all OpenGL function pointers
+  // ---------------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD!" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Registration of callback functions is done after window creation and before
+  // the render loop initiation.
+  // ---------------------------------------------------------------------------
+
+  // Render loop
+  // -----------
+  while (!glfwWindowShouldClose(window)) {
+    // Input
+    // -----
+    processInput(window);
+
+    // Rendering
+    // ---------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Swap buffers and poll IO events (key press/release, mouse events, etc.)
+    // -----------------------------------------------------------------------
+    glfwSwapBuffers(window); // swap between front and back buffers? to prevent
+                             // render flickering issues
+    glfwPollEvents();
+  }
+
+  // Clean up GLFW resources
+  // -----------------------
+  glfwTerminate();
+  return 0;
 }
 
 // Process all input
 // -----------------
-void processInput(GLFWwindow *window)
+void
+processInput(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    {
-        std::cout << "processInput(): closing window." << std::endl;
-        glfwSetWindowShouldClose(window, true);
-    }
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    std::cout << "processInput(): closing window." << std::endl;
+    glfwSetWindowShouldClose(window, true);
+  }
 }
 
 // GLFW: Callbacks
 // ---------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    std::cout << "framebuffer_size_callback(): (width: " << width << ", height: " << height << ")" << std::endl;
+  std::cout << "framebuffer_size_callback(): (width: " << width
+            << ", height: " << height << ")" << std::endl;
 
-    // GLint x, GLint y = set locn of the lower left corner of the window.
-    // GLsizei width, GLsizei height = set rendering viewport dimensions in pixels (can be smaller than Window dimensions).
-    glViewport(0, 0, width, height);
+  // GLint x, GLint y = set locn of the lower left corner of the window.
+  // GLsizei width, GLsizei height = set rendering viewport dimensions in pixels
+  // (can be smaller than Window dimensions).
+  glViewport(0, 0, width, height);
 }
